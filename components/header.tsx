570b706da0cefd9ab006c812f816400e37efe273
pablitto25 <pablitto18@gmail.com>
2024-08-30 10:51:39 -0300

    hotfix header

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export const Header = () => {
 
   const currentYear = new Date().getFullYear();
-  
+
 
   return (
     <header className="relative">
@@ -16,21 +16,19 @@ export const Header = () => {
             src={img.Imgs.headerLogo}
             alt="logo latamly header"
           />
-          <div className="absolute top-1/2 left-0 transform -translate-y-1/2 p-4 text-black">
+          <div className="absolute top-1/3 left-0 transform -translate-y-1/2 p-4 text-black">
             <h1 className="text-lg text-white font-bold">
               Satisfaction survey {currentYear}
             </h1>
             <h1 className="text-lg text-white">Latamly Group</h1>
           </div>
-          <div className="absolute top-1/3 right-[-400] transform -translate-y-1/2 p-4 text-black">
+          <div className="absolute top-1/3 right-[-300px] transform -translate-y-1/2 p-4 text-black">
             <div>
-              <h1 className="text-xl text-white font-light italic">
-                Language
-              </h1>
+              <h1 className="text-xl text-white font-light italic">Language</h1>
             </div>
             <div className="flex justify-center items-center gap-2 pt-2">
               <div className="w-9 bg-white rounded-lg text-center">
-                <Link href={"/partner-manufacturers"} className="text-[#FF0000] text-lg ">EN </Link>
+                <Link href={"/partner-manufacturers"} className="text-[#FF0000] text-lg">EN</Link>
               </div>
               <div className="w-9 border-solid border-2 border-white rounded-lg text-center">
                 <Link href={"/partner-manufacturers-es"} className="text-white">ES</Link>
